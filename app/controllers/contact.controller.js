a131d83ab9acb220e3413515f3d5d88fc6cad024
Quoc-Khai <KhaiCK22V7K509@vlvh.ctu.edu.vn>
2024-03-09 10:03:08 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,27 @@
+exports.create = (req, res) => {
+    res.send({ message: "create handler" });
+};
+
+exports.findALl = (req, res) => {
+    res.send({ message: "findALL handler" });
+};
+
+exports.FindOne = (req, res) => {
+    res.send({ message: "FindOne handler" });
+};
+
+exports.update = (req, res) => {
+    res.send({ message: "update handler" });
+};
+
+exports.delete = (req, res) => {
+    res.send({ message: "delete handler" });
+};
+
+exports.deleteALL = (req, res) => {
+    res.send({ message: "deleteALL handler" });
+};
+
+exports.findALLFavorite = (req, res) => {
+    res.send({ message: "findALLFavorite handler" });
+};
